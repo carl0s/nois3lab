14d1957c50747c9c7582379211781cdebe1f73f3
Carlo Frinolli <carlo@nois3lab.it>
2013-01-03 13:37:27 +0100

    fixed seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,6 +16,6 @@ Service.create(name: 'web')
 Service.create(name: 'events')
 Service.create(name: 'communication')
 
-Teammate.create(fullname: 'Nome Cognome', bio: 'dummy bio', twitter: '@dummy', facebook: 'name.lastname', linkedin: 'test')
+Teammate.create(fullname: 'Nome Cognome', bio: 'dummy bio', twitter: 'dummy', facebook: 'name.lastname', linkedin: 'test')
 
 Client.create(name: 'Test Client', content: 'Dummy content', url: 'http://www.example.com')
